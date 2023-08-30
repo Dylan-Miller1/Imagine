48614691eba658c87b8407aa74749d875e0c7b35
Dylan-Miller1 <134555676+Dylan-Miller1@users.noreply.github.com>
2023-08-30 15:47:59 -0700

    Removed SQlite. Added MySQL

--- app/database_interaction.py
+++ app/database_interaction.py
@@ -1,49 +1,30 @@
-import sqlite3
+import pymysql
+import os
 import datetime
+
+#RDS Client
+instance = os.environ["mysql_instance"]
+username = os.environ["mysql_username"]
+password = os.environ["mysql_password"]
+endpoint = os.environ["mysql_endpoint"]
+port = os.environ["mysql_port"]
+
+db = pymysql.connect(host=endpoint, user=username, password=password, db=instance)
+
+cursor = db.cursor()
 
 #Write prompt and keywords to database
 def db_write(prompt, chatgpt_response):
-    #Connect to database
-    connection = sqlite3.connect("prompts.db")
-
-    #Create cursor to database connection to execute commands
-    cursor = connection.cursor()
-
-    #Create Tuple to insert into table
     db_input = (str(datetime.datetime.now()), prompt, chatgpt_response)
-
-    #Insert user date, prompt and response into database (no duplicates)
-    cursor.execute("SELECT COUNT(*) FROM prompts WHERE user_keywords = ?", (db_input[1],))
-    count = cursor.fetchone()
-    if count[0] == 0:
-        cursor.execute("INSERT INTO prompts VALUES (?,?,?)", db_input)
-
-    #If db has more than 20 entries delete the oldest
-    cursor.execute("SELECT COUNT(*) FROM prompts")
-    number_entries = cursor.fetchone()[0]
-    if number_entries > 20:
-        cursor.execute("SELECT * FROM prompts ORDER BY entry_date ASC LIMIT 1")
-        oldest_prompt = cursor.fetchone()
-        oldest_id = oldest_prompt[0]
-        cursor.execute("DELETE FROM prompts WHERE entry_date = ?", (oldest_id,))
-        
-    #Commit changes to database
-    connection.commit()
-
-    #Close database connection
-    connection.close()
+    cursor.execute("INSERT INTO imagine_prompts VALUES (%s,%s,%s)", db_input)
+    db.commit()
 
 #Pull 20 most recent items in database
 def db_get_recent():
-    #Connect to database
-    connection = sqlite3.connect("prompts.db")
+    cursor.execute("SELECT * FROM imagine_prompts ORDER BY datesubmitted DESC LIMIT 100")
+    saved_prompts = cursor.fetchall()
+    print(saved_prompts)
+    return saved_prompts
 
-    #Create cursor to database connection to execute commands
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT * FROM prompts ORDER BY entry_date DESC LIMIT 20")
-    last_ten = cursor.fetchall()
-
-    #Close database connection
-    connection.close()
-    return last_ten
+cursor.close()
+db.close()
